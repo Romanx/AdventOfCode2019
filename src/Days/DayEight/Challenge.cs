b7bcb1a65f546899a52990cebe16e57342ccf8d3
Romanx <mcauliffe.alex@gmail.com>
2019-12-08 13:56:47 +0000

    Day Eight

---
+++ src/Days/DayEight/Challenge.cs
@@ -0,0 +1,68 @@
+﻿using Helpers;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace DayEight
+{
+    public class Challenge : ChallengeBase, INeedAllInput
+    {
+        public Challenge() : base(day: 8)
+        {
+        }
+
+        public override string Name => "Space Image Format";
+
+        public void PartOne(string input, TextWriter @out)
+        {
+            var image = Image.ParseSpaceImageFormat(input, 25, 6);
+
+            var layer = image.Layers
+                .OrderBy(layer => layer.Flatten().Count(i => i == 0))
+                .First();
+
+            var layerArray = layer.Flatten().ToArray();
+            var oneDigits = layerArray.Count(i => i == 1);
+            var twoDigits = layerArray.Count(i => i == 2);
+
+            @out.WriteLine($"One Digits {oneDigits}, Two Digits {twoDigits}: {oneDigits * twoDigits}");
+        }
+
+        public void PartTwo(string input, TextWriter @out)
+        {
+            var image = Image.ParseSpaceImageFormat(input, 25, 6);
+
+            Image squashed = image.Squash();
+            PrintImage(squashed);
+        }
+
+        public void PrintImage(Image image)
+        {
+            var pixels = image.Layers[0].Pixels;
+
+            for (var y = 0; y < image.Height; y++)
+            {
+                for (var x = 0; x < image.Width; x++)
+                {
+                    switch (pixels[y, x])
+                    {
+                        case 0:
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Write(" ");
+                            break;
+                        case 1:
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.Write(" ");
+                            break;
+                        case 2:
+                            Console.BackgroundColor = ConsoleColor.Gray;
+                            Console.Write(" ");
+                            break;
+                    }
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ src/Days/DayEight/Image.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace DayEight
+{
+    public class Image
+    {
+        public ImmutableArray<Layer> Layers { get; }
+        public int Width { get; }
+        public int Height { get; }
+
+        private Image(ImmutableArray<Layer> layers, int width, int height)
+        {
+            Layers = layers;
+            Width = width;
+            Height = height;
+        }
+
+        public static Image ParseSpaceImageFormat(string input, int width, int height)
+        {
+            var span = input.Trim()
+                .AsSpan();
+
+            var builder = ImmutableArray.CreateBuilder<Layer>();
+
+            while (!span.IsEmpty)
+            {
+                var layerPixels = new int[height, width];
+                for (var y = 0; y < height; y++)
+                {
+                    for (var x = 0; x < width; x++)
+                    {
+                        layerPixels[y, x] = span[x + (y * width)] - '0';
+                    }
+                }
+
+                builder.Add(new Layer(layerPixels));
+                span = span.Slice(width * height);
+            }
+
+            return new Image(builder.ToImmutable(), width, height);
+        }
+
+        internal Image Squash()
+        {
+            var builder = ImmutableArray.CreateBuilder<Layer>();
+            var layerPixels = new int[Height, Width];
+            ZeroArray(layerPixels);
+
+            foreach (var layer in Layers.Reverse())
+            {
+                for (var y = 0; y < Height; y++)
+                {
+                    for (var x = 0; x < Width; x++)
+                    {
+                        layerPixels[y, x] = layer.Pixels[y, x] switch
+                        {
+                            0 => 0,
+                            1 => 1,
+                            2 => layerPixels[y, x],
+                            _ => throw new InvalidOperationException("Invalid Pixel Combination"),
+                        };
+                    }
+                }
+            }
+
+            builder.Add(new Layer(layerPixels));
+            return new Image(builder.ToImmutable(), Width, Height);
+
+            static void ZeroArray(int[,] array)
+            {
+                for (int x = 0; x < array.GetLength(0); x += 1)
+                {
+                    for (int y = 0; y < array.GetLength(1); y += 1)
+                    {
+                        array[x, y] = 2;
+                    }
+                }
+            }
+        }
+
+        public class Layer
+        {
+            public Layer(int[,] pixels)
+            {
+                Pixels = pixels;
+            }
+
+            public int[,] Pixels { get; }
+
+            public IEnumerable<int> Flatten()
+            {
+                foreach (var item in Pixels)
+                    yield return item;
+            }
+        }
+    }
+}
