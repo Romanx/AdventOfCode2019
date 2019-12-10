b25a66d5bd0d4b77b859eb19729820005d80fe63
Romanx <mcauliffe.alex@gmail.com>
2019-12-10 16:39:34 +0000

    Break out shortest path class

--- src/Helpers/Graph/BreadthFirstSearch.cs
+++ src/Helpers/Graph/BreadthFirstSearch.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Helpers.Graph
@@ -34,55 +33,6 @@ namespace Helpers.Graph
             }
 
             return visited;
-        }
-    }
-
-    public static class ShortestPath
-    {
-        public static string[] Search(Graph graph, string start, string end)
-        {
-            var shortestFunc = BuildShortestFunction(graph, start);
-
-            return shortestFunc(end);
-        }
-
-        private static Func<string, string[]> BuildShortestFunction(Graph graph, string start)
-        {
-            var previous = new Dictionary<string, string>();
-
-            var queue = new Queue<string>();
-            queue.Enqueue(start);
-
-            while (queue.Count > 0)
-            {
-                var vertex = queue.Dequeue();
-                
-                foreach (var neighbour in graph.AdjacentList[vertex])
-                {
-                    if (previous.ContainsKey(neighbour))
-                        continue;
-
-                    previous[neighbour] = vertex;
-                    queue.Enqueue(neighbour);
-                }
-            }
-
-            return (target) =>
-            {
-                var path = new List<string>();
-
-                var current = target;
-                while (current != start)
-                {
-                    path.Add(current);
-                    current = previous[current];
-                }
-
-                path.Add(start);
-
-                path.Reverse();
-                return path.ToArray();
-            };
         }
     }
 }
